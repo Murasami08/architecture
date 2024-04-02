738457648780dc598056923b83ebe05214b7e157
Murasami08 <Murasami08@yandex.ru>
2024-04-02 16:50:40 +0300

    commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// alert("Welcome")
+window.onload = function() {
+
+	setTimeout(function() {
+
+		document.body.classList.add('loaded')
+
+		if (window.matchMedia('(min-width: 992px)').matches) { 
+
+			Draggable.create('.gallery', {
+				bounds: 'body',
+				inertia: true
+			})
+			
+		}
+
+	}, 200)
+
+}
